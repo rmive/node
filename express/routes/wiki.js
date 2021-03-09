544b4efab9d04ade1c3ef318b82e8ae04f895fd0
rirong <rirong@Chuns-MacBook-Pro.local>
2021-03-09 16:40:34 +0800

    finish 5.4 reading.

--- express/routes/wiki.js
+++ express/routes/wiki.js
@@ -2,6 +2,7 @@ var express = require('express');
 const { route } = require('.');
 var router = express.Router();
 
+
 router.get('/', function(req, res) {
     res.send('Wiki home page');
 });
@@ -14,5 +15,11 @@ router.post('/about', function(req, res) {
     res.send('About this wiki');
 })
 
+
+router.get('/:item', function(req, res) {
+    res.send(
+        '<h2> hello, ' + req.params.item + '</h2>'
+    );
+})
 
 module.exports = router;
